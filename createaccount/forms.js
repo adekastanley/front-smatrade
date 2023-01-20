adf4b6cbab5dcc66cfa59664fda4fb710be505cd
adekastanley <adekastanley2@gmail.com>
2023-01-20 19:45:24 +0100

    first commit

---
+++ createaccount/forms.js
@@ -0,0 +1,36 @@
+/*
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBH6uMvgK_tWtbe6XOIqlcVh8FuyobE4bs",
+    authDomain: "db-personal-92a43.firebaseapp.com",
+    databaseURL: "https://db-personal-92a43-default-rtdb.firebaseio.com",
+    projectId: "db-personal-92a43",
+    storageBucket: "db-personal-92a43.appspot.com",
+    messagingSenderId: "927490044428",
+    appId: "1:927490044428:web:77af7716b1cc4592a4b429"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
+*/
+
+// const firebaseConfig = {
+//     apiKey: "AIzaSyBH6uMvgK_tWtbe6XOIqlcVh8FuyobE4bs",
+//     authDomain: "db-personal-92a43.firebaseapp.com",
+//     databaseURL: "https://db-personal-92a43-default-rtdb.firebaseio.com",
+//     projectId: "db-personal-92a43",
+//     storageBucket: "db-personal-92a43.appspot.com",
+//     messagingSenderId: "927490044428",
+//     appId: "1:927490044428:web:77af7716b1cc4592a4b429"
+//   };
+
+
+// firebase.initializeApp(firebaseConfig);
+
